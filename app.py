448219777b868c32dd87e807b8d5704defee2854
Cristhian Murcia <cristhianmurcia182@gmail.com>
2020-02-11 06:47:24 -0500

    Starting point, testing the external libraries before coding logic

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request
+from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import create_engine
+
+from config import WebConfiguration
+
+app = Flask(__name__)
+
+SQL_SERVER_URL =  f"mssql+pyodbc://{WebConfiguration.DB_USER_NAME}:" \
+                                        f"{WebConfiguration.DB_PASSWORD}@localhost:1433/" \
+                                        f"{WebConfiguration.DB_NAME}?driver=SQL+Server"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = SQL_SERVER_URL
+
+db = SQLAlchemy(app)
+api = Api(app)
+
+
+
+
+db_core = create_engine(SQL_SERVER_URL, strategy='threadlocal') if SQL_SERVER_URL else None
+
+class Products(Resource):
+    def get(self):
+        return {'books_count': 25}
+
+    def post(self):
+        json = request.get_json()
+        return {'you_sent': json}
+
+class Users(Resource):
+    def get(self, num):
+        return {'result': num*10}
+
+api.add_resource(Users, '/user/<int:num>')
+api.add_resource(Products, '/products')
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ config.py
@@ -0,0 +1,4 @@
+class WebConfiguration:
+    DB_USER_NAME = 'sa'
+    DB_NAME = 'store'
+    DB_PASSWORD = 'interstella555*'
