992c7cd2c7ad4e4595156f271e9dea9c28bf7f93
Cristhian Murcia <cristhianmurcia182@gmail.com>
2020-02-12 22:21:19 -0500

    Removed unused commented lines in serializer file

--- serializers.py
+++ serializers.py
@@ -48,21 +48,3 @@ class ReceiptSchema(ma.Schema):
                 elem['client_name'] = elem.get('client', {}).get('name')
                 del elem['client']
         return data
-
-
-#
-#
-# class BankSAccountTypeSchema(ma.Schema):
-#     id = fields.Integer(dump_only=True)
-#     name = fields.String(required=True)
-#
-#
-# class BankAccSchema(ma.Schema):
-#     id = fields.Integer()
-#     account_number = fields.String(required=True)
-#
-#     account_type_id = fields.Integer(required=True, load_only=True)
-#     account_type = fields.Nested(BankSAccountTypeSchema, required=True, dump_only=True)
-#     bank_id = fields.Integer(required=True, load_only=True)
-#     bank = fields.Nested(BankSchema, required=True, dump_only=True)
-#     suscribed = fields.Boolean(default=False, data_key='subscribed')
